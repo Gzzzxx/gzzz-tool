f8371c15847daff3b5d5e7b35e634553548b09c9
gzzz <405227329@qq.com>
2023-10-20 13:48:58 +0800

    update

---
+++ src/main/java/art/gzzz/web/service/Sm4Service.java
@@ -0,0 +1,26 @@
+package art.gzzz.web.service;
+
+import art.gzzz.web.domain.request.Sm4Request;
+import art.gzzz.web.domain.response.Sm4Response;
+
+/**
+ * @author gz
+ */
+public interface Sm4Service {
+
+  /**
+   * encrypt
+   * @param request request
+   * @return Sm4Response
+   * @throws Exception exception
+   */
+  Sm4Response encrypt(Sm4Request request) throws Exception;
+
+  /**
+   * decrypt
+   * @param request request
+   * @return Sm4Response
+   * @throws Exception exception
+   */
+  Sm4Response decrypt(Sm4Request request) throws Exception;
+}
